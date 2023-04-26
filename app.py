b42e36f9916da92755853f79328cdddaab38bfa7
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-04-26 17:56:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
 import requests
-import streamlit as st
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
-def scrape_sitemap(sitemap_url, target_url, keyword):
-    sitemap = requests.get(sitemap_url)
-    tree = ET.fromstring(sitemap.content)
-    matching_urls = []
-    for url in tree.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}url'):
-        loc = url.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc').text
-        if keyword in requests.get(loc).text and target_url not in requests.get(loc).text:
-            matching_urls.append(loc)
-    return matching_urls
+def check_url(url, keyword, target_url):
+    try:
+        # Get the HTML content of the URL
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Check if the HTML contains the keyword
+        if keyword not in soup.get_text():
+            return None
+        
+        # Check if the HTML contains a link to the target URL
+        parsed_target_url = urlparse(target_url)
+        for link in soup.find_all('a'):
+            parsed_link = urlparse(link.get('href'))
+            if parsed_target_url.netloc == parsed_link.netloc and parsed_target_url.path == parsed_link.path:
+                return None
+        
+        # If the URL passes all checks, return it
+        return url
+    
+    except Exception as e:
+        print(f"Error checking URL {url}: {e}")
+        return None
 
-st.title('Sitemap Scraper')
+def search_sitemap(sitemap_url, keyword, target_url):
+    # Get the XML sitemap
+    response = requests.get(sitemap_url)
+    soup = BeautifulSoup(response.content, 'xml')
 
-sitemap_url = st.text_input('Enter the URL of the sitemap:')
-target_url = st.text_input('Enter the URL of the target site:')
-keyword = st.text_input('Enter a keyword to search for in the sitemap URLs:')
+    # Find all the URL tags in the sitemap
+    urls = soup.find_all('url')
 
-if st.button('Search'):
-    matching_urls = scrape_sitemap(sitemap_url, target_url, keyword)
-    st.write(f'The following {len(matching_urls)} URLs match your search criteria:')
-    for url in matching_urls:
-        st.write(url)
+    # Check each URL for the keyword and target URL
+    results = []
+    for url in urls:
+        loc = url.find('loc').text
+        result = check_url(loc, keyword, target_url)
+        if result:
+            results.append(result)
+    
+    return results
+
+# Example usage
+sitemap_url = 'https://example.com/sitemap.xml'
+keyword = 'example'
+target_url = 'https://example.com/page-to-exclude'
+results = search_sitemap(sitemap_url, keyword, target_url)
+print(results)
