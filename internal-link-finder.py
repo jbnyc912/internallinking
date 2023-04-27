00d29ecc8e1d4b6eaee344cc541dcc2b1a76ab1c
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-04-27 06:27:47 -0600

    Update internal-link-finder.py

--- internal-link-finder.py
+++ internal-link-finder.py
@@ -4,11 +4,12 @@ import streamlit as st
 import pandas as pd
 import base64
 
+
 def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
-    st.write("Crawling...")
     passed_urls = []
-    for i, url in enumerate(site_urls):
-        st.write(f"Crawling {i+1} out of {len(site_urls)}")
+    num_crawled = 0
+    num_passed = 0
+    for url in site_urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
         keyword_found = False
@@ -22,28 +23,39 @@ def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
             href = link.get("href")
             if href is not None and (target_url in href or href.startswith(target_url)):
                 passed_urls.append(url)
+                num_passed += 1
                 break
-    st.write(f"There are {len(passed_urls)} internal linking opportunities.")
-    return passed_urls
+        num_crawled += 1
+        st.text(f"Crawling {num_crawled} out of {len(site_urls)}...")
+    return passed_urls, num_passed
+
 
 def main():
-    st.set_page_config(page_title="Internal Linking Finder", page_icon=":link:")
-
-    st.title("Internal Linking Finder")
-    st.markdown("***")
-    
-    site_urls = st.text_area("Site URLs\n\n_Paste URLs below, one per line_")
+    st.title("**Internal Linking Finder**")
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.subheader("**Site URLs**")
+    st.markdown("*Paste URLs below, one per line*", unsafe_allow_html=True)
+    site_urls = st.text_area("", "https://www.google.com\nhttps://www.github.com", height=150)
     site_urls = site_urls.split("\n")
-    
-    keywords = st.text_area("Keywords\n\n_Paste relevant keywords or terms below, one per line_")
+    st.subheader("**Keywords**")
+    st.markdown("*Paste relevant keywords or terms below, one per line*", unsafe_allow_html=True)
+    keywords = st.text_area("", "Python\nStreamlit\nWeb scraping", height=150)
     keywords = keywords.split("\n")
-    
-    target_url = st.text_input("Target URL\n\n_Target URL you're looking to add internal links to_")
-    
+    st.subheader("**Target URL**")
+    st.markdown("*Target URL you're looking to add internal links to*", unsafe_allow_html=True)
+    target_url = st.text_input("", "https://www.example.com")
     if st.button("Run Crawler"):
-        passed_urls = find_urls_with_keywords_and_target(site_urls, keywords, target_url)
+        passed_urls, num_passed = find_urls_with_keywords_and_target(site_urls, keywords, target_url)
+        st.success(f"Finished crawling {len(site_urls)} sites. Found {num_passed} internal linking opportunities.")
         if passed_urls:
-            st.write(f"Download {len(passed_urls)} internal linking opportunities as CSV:")
+            st.markdown("<br>", unsafe_allow_html=True)
+            st.subheader("**URLs that passed all checks**")
+            for passed_url in passed_urls:
+                st.write(passed_url)
+            
+            # Export results to CSV
+            st.markdown("<br>", unsafe_allow_html=True)
             data = {"URL": passed_urls, "Keyword": [", ".join(keywords)] * len(passed_urls)}
             df = pd.DataFrame(data)
             csv = df.to_csv(index=False)
@@ -51,7 +63,8 @@ def main():
             href = f'<a href="data:file/csv;base64,{b64}" download="results.csv"><button>Download CSV</button></a>'
             st.markdown(href, unsafe_allow_html=True)
         else:
-            st.write("No URLs passed all checks.")
+            st.warning("No URLs passed all checks.")
+
 
 if __name__ == "__main__":
     main()
