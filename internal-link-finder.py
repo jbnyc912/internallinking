180acc13a62676c28e68358141ea7f7a5aa850cc
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-04-27 07:09:43 -0600

    Update internal-link-finder.py

--- internal-link-finder.py
+++ internal-link-finder.py
@@ -4,12 +4,12 @@ import streamlit as st
 import pandas as pd
 import base64
 
-
 def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
     passed_urls = []
     num_crawled = 0
     num_passed = 0
-    progress_bar = st.progress(0)
+    progress_text = st.sidebar.empty()
+    progress_bar = st.sidebar.progress(0)
     for i, url in enumerate(site_urls):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
@@ -27,8 +27,8 @@ def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
                 num_passed += 1
                 break
         num_crawled += 1
-        progress_bar.progress((i + 1) / len(site_urls))
-        st.sidebar.text(f"Crawling {i+1}/{len(site_urls)}...")
+        progress_text.text(f"Crawling {i+1} out of {len(site_urls)}...")
+        progress_bar.progress(int((i+1)/len(site_urls)*100))
     return passed_urls, num_passed
 
 
@@ -38,36 +38,35 @@ def main():
     st.markdown("<br>", unsafe_allow_html=True)
     st.subheader("**Site URLs**")
     st.markdown("*Paste URLs below, one per line*", unsafe_allow_html=True)
-    site_urls = st.text_area("", placeholder="https://www.google.com\nhttps://www.github.com", height=150)
+    site_urls = st.text_area("", "https://www.google.com\nhttps://www.github.com", height=150, key='site_urls')
     site_urls = site_urls.split("\n")
     st.subheader("**Keywords**")
     st.markdown("*Paste relevant keywords or terms below, one per line*", unsafe_allow_html=True)
-    keywords = st.text_area("", placeholder="Python\nStreamlit\nWeb scraping", height=150)
+    keywords = st.text_area("", "Python\nStreamlit\nWeb scraping", height=150, key='keywords')
     keywords = keywords.split("\n")
     st.subheader("**Target URL**")
     st.markdown("*Target URL you're looking to add internal links to*", unsafe_allow_html=True)
-    target_url = st.text_input("", placeholder="https://www.example.com")
+    target_url = st.text_input("", "https://www.example.com", key='target_url')
     if st.button("Run Crawler"):
         passed_urls, num_passed = find_urls_with_keywords_and_target(site_urls, keywords, target_url)
         st.success(f"Finished crawling {len(site_urls)} sites. Found {num_passed} internal linking opportunities.")
         if passed_urls:
+            st.markdown("<br>", unsafe_allow_html=True)
+            st.subheader("**URLs that passed all checks**")
+            for passed_url in passed_urls:
+                st.write(passed_url)
+            
             # Export results to CSV
             st.markdown("<br>", unsafe_allow_html=True)
-            data = {"URL": passed_urls, "Keyword": []}
-            for url in passed_urls:
-                response = requests.get(url)
-                soup = BeautifulSoup(response.content, "html.parser")
-                keywords_found = []
-                for keyword in keywords:
-                    if keyword.lower() in soup.get_text().lower():
-                        keywords_found.append(keyword)
-                data["Keyword"].append(", ".join(keywords_found))
+            data = {"URL": passed_urls, "Keyword": [", ".join(keywords)] * len(passed_urls)}
             df = pd.DataFrame(data)
-            csv_title = f"Internal Linking - {target_url}"
             csv = df.to_csv(index=False)
             b64 = base64.b64encode(csv.encode()).decode()
-            href = f'<a href="data:file/csv;base64,{b64}" download="{csv_title}.csv"><button>Download CSV</button></a>'
+            filename = f"Internal Linking - {target_url}.csv"
+            href = f'<a href="data:file/csv;base64,{b64}" download="{filename}"><button>Download CSV</button></a>'
             st.markdown(href, unsafe_allow_html=True)
-            
+        else:
+            st.warning("No URLs passed all checks.")
+
 if __name__ == "__main__":
     main()
