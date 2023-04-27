d363a0542de9d89ba5d56c76cb62e79c258fca45
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-04-27 07:28:36 -0600

    Update internal-link-finder.py

--- internal-link-finder.py
+++ internal-link-finder.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import base64
 
+
 def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
     passed_urls = []
     num_crawled = 0
@@ -20,16 +21,17 @@ def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
                 break
         if not keyword_found:
             continue
-        for link in soup.find_all("a"):
-            href = link.get("href")
-            if href is not None and (target_url in href or href.startswith(target_url)):
-                passed_urls.append(url)
-                num_passed += 1
-                break
+        keywords_on_page = []
+        for keyword in keywords:
+            if keyword.lower() in soup.get_text().lower():
+                keywords_on_page.append(keyword)
+        keywords_on_page_str = ', '.join(keywords_on_page)
+        passed_urls.append({'URL': url, 'Keywords Found': keywords_on_page_str})
+        num_passed += 1
         num_crawled += 1
         progress_text.text(f"Crawling {i+1} out of {len(site_urls)}...")
         progress_bar.progress(int((i+1)/len(site_urls)*100))
-    return passed_urls, num_passed
+    return passed_urls
 
 
 def main():
@@ -48,18 +50,26 @@ def main():
     st.markdown("*Target URL you're looking to add internal links to*", unsafe_allow_html=True)
     target_url = st.text_input("", placeholder="https://www.example.com")
     if st.button("Run Crawler"):
-        passed_urls, num_passed = find_urls_with_keywords_and_target(site_urls, keywords, target_url)
-        st.success(f"Finished crawling {len(site_urls)} sites. Found {num_passed} internal linking opportunities.")
-            
-        # Export results to CSV
-        st.markdown("<br>", unsafe_allow_html=True)
-        data = {"URL": passed_urls, "Keyword": [", ".join(keywords)] * len(passed_urls)}
-        df = pd.DataFrame(data)
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()
-        filename = f"Internal Linking - {target_url}.csv"
-        href = f'<a href="data:file/csv;base64,{b64}" download="{filename}"><button>Download CSV</button></a>'
-        st.markdown(href, unsafe_allow_html=True)
+        passed_urls = find_urls_with_keywords_and_target(site_urls, keywords, target_url)
+        st.success(f"Finished crawling {len(site_urls)} sites. Found {len(passed_urls)} internal linking opportunities.")
+        if passed_urls:
+            # Export results to CSV
+            st.markdown("<br>", unsafe_allow_html=True)
+            st.subheader("**Export Results to CSV**")
+            st.write("Click the button below to export results to CSV:")
+            data = {'URL': [], 'Keywords Found': []}
+            for url in passed_urls:
+                data['URL'].append(url['URL'])
+                data['Keywords Found'].append(url['Keywords Found'])
+            df = pd.DataFrame(data)
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            filename = f"Internal Linking - {target_url}.csv"
+            href = f'<a href="data:file/csv;base64,{b64}" download="{filename}"><button>Download CSV</button></a>'
+            st.markdown(href, unsafe_allow_html=True)
+        else:
+            st.warning("No URLs passed all checks.")
+
 
 if __name__ == "__main__":
-    main()
+   
