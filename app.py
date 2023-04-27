9eea5d4ef3b54c7589ffd4be72b41bd577915c91
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-04-26 18:24:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,31 @@
-# Import the necessary libraries
 import streamlit as st
 import requests
-pip install lxml
 from lxml import etree
 
-# Define the Streamlit app
 def app():
-    # Set the title
     st.title("Web Scraper")
-    
-    # Define the input fields
+
     sitemap_url = st.text_input("Enter the sitemap.xml URL:")
     keyword = st.text_input("Enter the keyword to search for:")
     target_url = st.text_input("Enter the target URL to avoid:")
-    
-    # Define a function to scrape the URLs
+
     def scrape_urls(sitemap_url, keyword, target_url):
-        # Get the sitemap
         res = requests.get(sitemap_url)
         root = etree.fromstring(res.content)
-        
-        # Get the URLs from the sitemap
-        urls = [loc.text for loc in root.findall("{http://www.sitemaps.org/schemas/sitemap/0.9}url/{http://www.sitemaps.org/schemas/sitemap/0.9}loc")]
-        
-        # Loop through the URLs
+        urls = [loc.text for loc in root.xpath(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")]
+
         for url in urls:
-            # Get the HTML of the URL
             res = requests.get(url)
-            if res.status_code == 200:
-                # Check if the keyword is in the HTML
-                if keyword in res.text:
-                    # Check if the target URL is not in the HTML
-                    if target_url not in res.text:
-                        # Return the URL
-                        return url
-        
-        # If no URL is found, return None
+            root = etree.fromstring(res.content)
+            if keyword in root.xpath(".//body//text()"):
+                if target_url not in root.xpath(".//a/@href"):
+                    return url
+
         return None
-    
-    # Define the output
+
     if st.button("Scrape"):
         url = scrape_urls(sitemap_url, keyword, target_url)
-        
+
         if url:
             st.success(f"Found URL: {url}")
         else:
