df15b1ef2ab5b2d8559c6f1dd0ccf75352f665f2
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-04-26 18:06:45 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+# Import the necessary libraries
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+
+# Define the Streamlit app
+def app():
+    # Set the title
+    st.title("Web Scraper")
+    
+    # Define the input fields
+    sitemap_url = st.text_input("Enter the sitemap.xml URL:")
+    keyword = st.text_input("Enter the keyword to search for:")
+    target_url = st.text_input("Enter the target URL to avoid:")
+    
+    # Define a function to scrape the URLs
+    def scrape_urls(sitemap_url, keyword, target_url):
+        # Get the sitemap
+        res = requests.get(sitemap_url)
+        soup = BeautifulSoup(res.content, "xml")
+        
+        # Get the URLs from the sitemap
+        urls = [loc.text for loc in soup.find_all("loc")]
+        
+        # Loop through the URLs
+        for url in urls:
+            # Get the HTML of the URL
+            res = requests.get(url)
+            soup = BeautifulSoup(res.content, "html.parser")
+            
+            # Check if the keyword is in the HTML
+            if keyword in soup.text:
+                # Check if the target URL is not in the HTML
+                if target_url not in [a.get("href") for a in soup.find_all("a")]:
+                    # Return the URL
+                    return url
+        
+        # If no URL is found, return None
+        return None
+    
+    # Define the output
+    if st.button("Scrape"):
+        url = scrape_urls(sitemap_url, keyword, target_url)
+        
+        if url:
+            st.success(f"Found URL: {url}")
+        else:
+            st.warning("No URL found.")
