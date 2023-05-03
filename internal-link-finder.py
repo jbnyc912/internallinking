26afd6018c13453447784cd6f1e4f1459aa285fd
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-05-03 07:13:35 -0600

    Update internal-link-finder.py

--- internal-link-finder.py
+++ internal-link-finder.py
@@ -41,11 +41,10 @@ def find_urls_with_keywords_and_target(site_urls, keywords, target_url):
     return passed_urls
 
 def main():
-    st.set_page_config(page_title="Internal Linking Finder", page_icon=":link:")
+    st.set_page_config(page_title="Internal Linking Finder - a Break The Web tool", page_icon=":link:")
     st.image("https://scontent.fslc3-2.fna.fbcdn.net/v/t39.30808-6/306042676_506308304831092_90216115740552247_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=1fBuPeS-wTYAX9JSC05&_nc_ht=scontent.fslc3-2.fna&oh=00_AfAnvRo-0PBoKFOsSv_Lt8vbWf2gOz5kwvHEjlkd0GlM2Q&oe=6457BA63", width=40)
     st.title("Internal Linking Finder")
-    st.markdown("<div style='display: flex;align-items: center;'> <img src='https://i.imgur.com/wFfUZDQ.png' style='width: auto;height: 40px;margin-right: 5px;'> <p style='font-size: small;'>a Break The Web tool</p> </div>", unsafe_allow_html=True)
-    st.markdown("This tool allows you to identify URLs that don't currently link to the Target URL, and that mention the keyword(s).")
+    st.markdown("This tool allows you to identify URLs not current linking to the Target URL, and also include the keyword(s).")
 
     # CSV upload
     st.subheader("Site URLs")
