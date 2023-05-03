8b8f1af0f0ccbc54caa76d26a84a63641aa8e6e8
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2023-05-03 06:53:06 -0600

    Update internal-link-finder.py

--- internal-link-finder.py
+++ internal-link-finder.py
@@ -47,6 +47,15 @@ def main():
 
     # CSV upload
     st.subheader("Site URLs")
+    st.markdown("*Upload a CSV file below. The list of URLs should be in column A with no header.*", unsafe_allow_html=True)
+    st.markdown(
+        """<style>
+        .st-ca {
+            margin-top: -10px;
+        }
+        </style>""", 
+        unsafe_allow_html=True
+    )
     uploaded_file = st.file_uploader("", type="csv")
     site_urls = []
     if uploaded_file is not None:
