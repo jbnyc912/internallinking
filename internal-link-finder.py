862ff113e0feacb34220a60d08ef2fb789d561c8
jbnyc912 <91150156+jbnyc912@users.noreply.github.com>
2024-08-28 14:39:45 -0600

    Update internal-link-finder.py

--- internal-link-finder.py
+++ internal-link-finder.py
@@ -124,6 +124,12 @@ def main():
         reset_fields()
         st.experimental_rerun()
 
+st.markdown("""
+    This tool allows you to identify URLs not currently linking to the Target URL, and also include the keyword(s). 
+    \n
+    For more details on how to use this tool, see the [guide](#how-to-use-the-internal-link-finder-tool) below.
+    """)
+    
     # Add guide
     st.markdown("---")
     st.markdown("""
